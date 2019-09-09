2474bb5448a891888f47ab2591331400cc7bff3f
Tinnakorn Choompee <tinnakornchoompee@gmail.com>
2019-09-09 12:14:21 +0700

    Add Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { VideoCenterComponent } from './video-center/video-center.component';
+import { HeaderComponent } from './templates/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    VideoCenterComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
